0b4e1ab7881f997e4df373e5b7c517b2a02777c1
aruaru0 <aruaru0@gmail.com>
2024-03-16 10:21:48 +0900

    240316

---
+++ etc/240316/a.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+
+using namespace std;
+using namespace atcoder;
+
+int main() {
+    int n;
+
+    cin >> n;
+
+    int ans = 1e8;
+    for(int i=0;i < n;i++) {
+        int cnt = 0;
+        int p;
+        cin >> p;
+        while(p%10 == 0) {
+            p /= 10;
+            cnt++;
+        }
+        ans = min(ans, cnt);
+    }
+
+    cout << ans << endl;
+}
