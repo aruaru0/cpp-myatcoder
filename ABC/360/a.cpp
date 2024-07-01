a62451bb1405d2f2d7ba3efe7190359c2636c702
aruaru0 <aruaru0@gmail.com>
2024-07-01 10:31:02 +0900

    abc360-A

---
+++ ABC/360/a.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    int r, m;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == 'R')
+        {
+            r = i;
+        }
+        if (s[i] == 'M')
+        {
+            m = i;
+        }
+    }
+    if (r < m)
+    {
+        cout << "Yes" << endl;
+    }
+    else
+    {
+        cout << "No" << endl;
+    }
+}
