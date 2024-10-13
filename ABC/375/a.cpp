23c19d9d9d1edae8bcc33a5447ac1656f01f5b15
aruaru0 <aruaru0@gmail.com>
2024-10-13 09:37:33 +0900

    abc375-A

---
+++ ABC/375/a.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    int cnt = 0;
+    for (int i = 0; i < n - 2; i++)
+    {
+        if (s[i] == '#' && s[i + 1] == '.' && s[i + 2] == '#')
+        {
+            cnt++;
+        }
+    }
+    cout << cnt << endl;
+}
