6a105522c2a3cd14173df0231a15d5b0ddb0571a
aruaru0 <aruaru0@gmail.com>
2025-03-02 09:55:59 +0900

    abc395-A

---
+++ ABC/395/a.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    for (int i = 1; i < n; i++)
+    {
+        if (a[i] <= a[i - 1])
+        {
+            cout << "No" << endl;
+            return 0;
+        }
+    }
+    cout << "Yes" << endl;
+    return 0;
+}
