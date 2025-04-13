51902604da166d30f071a89b3ad5605a2af492cd
aruaru0 <aruaru0@gmail.com>
2025-04-13 09:45:58 +0900

    abc401-A

---
+++ ABC/401/a.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int s;
+    cin >> s;
+
+    if (200 <= s && s <= 299)
+    {
+        cout << "Success" << endl;
+    }
+    else
+    {
+        cout << "Failure" << endl;
+    }
+}
