56add034e5a49115c27c710bb5309f650cd81a51
aruaru0 <aruaru0@gmail.com>
2025-04-17 14:15:23 +0900

    PAST018-C

---
+++ PAST/018/c.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int b1, r1, b2, r2, t;
+    cin >> b1 >> r1 >> b2 >> r2;
+    cin >> t;
+
+    int cnt = 0;
+    for (int i = 0; i < t; i++)
+    {
+        bool signal1 = false;
+        if (i % (b1 + r1) < b1)
+        {
+            signal1 = true;
+        }
+        bool signal2 = false;
+        if (i % (b2 + r2) < b2)
+        {
+            signal2 = true;
+        }
+        if (signal1 == signal2)
+        {
+            cnt++;
+        }
+    }
+    cout << cnt << endl;
+}
