54a7340aca5bf83a2e1fbb93f7504c0a4f527ecc
aruaru0 <aruaru0@gmail.com>
2026-03-15 10:04:42 +0900

    abc449-A

---
+++ ABC/449/a.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+// coutにvector<int>を表示させる
+template <class T>
+std::ostream &operator<<(std::ostream &os, const std::vector<T> &v)
+{
+    os << "[";
+    for (int i = 0; i < (int)v.size(); i++)
+    {
+        os << v[i] << (i + 1 == (int)v.size() ? "" : ", ");
+    }
+    os << "]";
+    return os;
+}
+
+int main()
+{
+    double d;
+    cin >> d;
+    double ans = M_PI * (d / 2) * (d / 2);
+    cout << std::setprecision(15) << ans << endl;
+
+    return 0;
+}
