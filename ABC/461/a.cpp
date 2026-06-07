30952cf318eb3e1561f38a976bec46e809ff6888
aruaru0 <aruaru0@gmail.com>
2026-06-07 09:33:19 +0900

    abc461-A

---
+++ ABC/461/a.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, d;
+    cin >> a >> d;
+    if (a > d)
+    {
+        cout << "No" << endl;
+    }
+    else
+    {
+        cout << "Yes" << endl;
+    }
+}
