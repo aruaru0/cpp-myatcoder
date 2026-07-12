4878c8a40dc3631ded29e1168c25f20f5ca442ec
aruaru0 <aruaru0@gmail.com>
2026-07-12 09:42:19 +0900

    abc466-A

---
+++ ABC/466/a.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        if (x >= 0)
+        {
+            cout << "No" << endl;
+            return 0;
+        }
+    }
+    cout << "Yes" << endl;
+    return 0;
+}
