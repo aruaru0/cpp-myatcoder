11f0a69e24e21197ea295b6ba3d1b6db883ea270
aruaru0 <aruaru0@gmail.com>
2026-08-16 09:40:32 +0900

    abc471-A

---
+++ ABC/471/a.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    if ((a + b == 9) || (a - b == 9) || (a * b == 9) || (a == b * 9))
+    {
+        cout << "Nine" << endl;
+    }
+    else
+    {
+        cout << "Nein" << endl;
+    }
+}
